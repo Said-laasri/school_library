ef4689cb7f1466e8e682f9d8f7cfdbbad530fb76
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-26 20:13:41 +0100

    Add parameter on student constractor

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom)
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
     super(name, age, parent_permission)
     @classroom = classroom
   end
