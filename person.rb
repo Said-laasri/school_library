1c1ab72ba085bbf230efc06357f09cf8cf23b964
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-26 15:47:15 +0100

    Fix rubocop ruby

--- person.rb
+++ person.rb
@@ -1,24 +1,20 @@
 class Person
-  attr_accessor :id, :name, :age, :parent_permission 
+  attr_accessor :id, :name, :age, :parent_permission
 
-  def initialize(name: "Unknown", age, parent_permission: true )
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  
+
   def can_use_services?
-    if @age >= 18 || @parent_permission
-      true
+    true if @age >= 18 || @parent_permission
   end
 
-  Private
-  def is_of_age?
-      if @age >= 18
-        true
-      else
-        false 
-      end
-  end
+  private
 
+  def of_age?
+    @age >= 18
+  end
 end
