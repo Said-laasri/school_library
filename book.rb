c5802c4da97fa38cb5a9ccfba45631f77c895eaa
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-28 12:49:10 +0100

    Add has-many and belongs to between book and rental

--- book.rb
+++ book.rb
@@ -1,8 +1,15 @@
 class Book
   attr_accessor :author, :title
+  attr_reader :rentals
 
   def initialize(author, title)
     @author = author
     @title = title
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.book = self
   end
 end
