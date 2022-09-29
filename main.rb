c9f89a0f7a715751b54276e493fb80da468728cf
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-29 16:58:57 +0100

    Add class App file and mainmethode file

--- main.rb
+++ main.rb
@@ -1,15 +1,44 @@
-require_relative 'person'
-require_relative 'capitalizedecorator'
-require_relative 'trimmerdecorator'
+require_relative 'app'
 
-person = Person.new(22, 'maximilianus')
+def selection(option, app)
+  case option
+  when 1
+    app.list_books
+  when 2
+    app.list_people
+  when 3
+    app.add_person
+  when 4
+    app.add_book
+  when 5
+    app.add_rental
+  when 6
+    app.list_rentals
+  when 7
+    app.exit_method
+  else
+    puts 'bye'
+  end
+end
 
-p person.correct_name
+def main
+  app = App.new
+  loop do
+    puts 'Welcome to School Library App!'
+    puts ' '
+    puts 'Please choose an option by entering a number'
+    puts ' 1 - List all books'
+    puts ' 2 - List all people'
+    puts ' 3 - Create a person'
+    puts ' 4 - Create a book'
+    puts ' 5 - Create a rental'
+    puts ' 6 - List all rental for a given person id'
+    puts ' 7 - Exit '
 
-capitalizedperson = CapitalizeDecorator.new(person)
+    option = gets.chomp.to_i
 
-p capitalizedperson.correct_name
+    selection(option, app)
+  end
+end
 
-capitalizedtrimmedperson = TrimmerDecorator.new(capitalizedperson)
-
-p capitalizedtrimmedperson.correct_name
+main
